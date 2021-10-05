16f41a434b73c634d1cc5028748dc9bf9a9952ea
Pitam Kumar Dey <85692326+Pitam18@users.noreply.github.com>
2021-10-05 13:33:09 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,132 @@
+ document.getElementById("fir").onclick=function ()
+ {
+    
+    document.getElementById("sec").style.display="none";
+    document.getElementById("three").style.display="none";
+    document.getElementById("four").style.display="none";
+    document.getElementById("five").style.display="none";
+    document.getElementById("six").style.display="none";
+    document.getElementById("seven").style.display="none";
+    
+    document.getElementById("a").style.display="block";
+    document.getElementById("b").style.display="block";
+    document.getElementById("c").style.display="block";
+    document.getElementById("d").style.display="block";
+    document.getElementById("e").style.display="block";
+    document.getElementById("f").style.display="block";
+   
+     
+}
+document.getElementById("sec").onclick=function ()
+ {
+    document.getElementById("fir").style.display="none";
+    
+    document.getElementById("three").style.display="none";
+    document.getElementById("four").style.display="none";
+    document.getElementById("five").style.display="none";
+    document.getElementById("six").style.display="none";
+    document.getElementById("seven").style.display="none";
+    
+    document.getElementById("g").style.display="block";
+    document.getElementById("h").style.display="block";
+    document.getElementById("i").style.display="block";
+    document.getElementById("j").style.display="block";
+    document.getElementById("k").style.display="block";
+    document.getElementById("l").style.display="block";
+   
+     
+}
+document.getElementById("three").onclick=function ()
+ {
+    document.getElementById("fir").style.display="none";
+    document.getElementById("sec").style.display="none";
+    
+    document.getElementById("four").style.display="none";
+    document.getElementById("five").style.display="none";
+    document.getElementById("six").style.display="none";
+    document.getElementById("seven").style.display="none";
+    
+    document.getElementById("m").style.display="block";
+    document.getElementById("n").style.display="block";
+    document.getElementById("o").style.display="block";
+    document.getElementById("p").style.display="block";
+    document.getElementById("q").style.display="block";
+    document.getElementById("r").style.display="block";
+   
+     
+}
+document.getElementById("four").onclick=function ()
+ {
+    document.getElementById("fir").style.display="none";
+    document.getElementById("sec").style.display="none";
+    document.getElementById("three").style.display="none";
+    
+    document.getElementById("five").style.display="none";
+    document.getElementById("six").style.display="none";
+    document.getElementById("seven").style.display="none";
+    
+    document.getElementById("s").style.display="block";
+    document.getElementById("t").style.display="block";
+    document.getElementById("u").style.display="block";
+    document.getElementById("v").style.display="block";
+    document.getElementById("w").style.display="block";
+    document.getElementById("x").style.display="block";
+   
+     
+}
+document.getElementById("five").onclick=function ()
+ {
+    document.getElementById("fir").style.display="none";
+    document.getElementById("sec").style.display="none";
+    document.getElementById("three").style.display="none";
+    document.getElementById("four").style.display="none";
+   
+    document.getElementById("six").style.display="none";
+    document.getElementById("seven").style.display="none";
+    
+    document.getElementById("y").style.display="block";
+    document.getElementById("z").style.display="block";
+    document.getElementById("aa").style.display="block";
+    document.getElementById("bb").style.display="block";
+    document.getElementById("cc").style.display="block";
+    document.getElementById("dd").style.display="block";
+   
+     
+}
+document.getElementById("six").onclick=function ()
+ {
+    document.getElementById("fir").style.display="none";
+    document.getElementById("sec").style.display="none";
+    document.getElementById("three").style.display="none";
+    document.getElementById("four").style.display="none";
+    document.getElementById("five").style.display="none";
+    
+    document.getElementById("seven").style.display="none";
+    
+    document.getElementById("ee").style.display="block";
+    document.getElementById("ff").style.display="block";
+    document.getElementById("gg").style.display="block";
+    document.getElementById("hh").style.display="block";
+    document.getElementById("ii").style.display="block";
+    document.getElementById("jj").style.display="block";
+   
+     
+}
+document.getElementById("seven").onclick=function ()
+ {
+    document.getElementById("fir").style.display="none";
+    document.getElementById("sec").style.display="none";
+    document.getElementById("three").style.display="none";
+    document.getElementById("four").style.display="none";
+    document.getElementById("five").style.display="none";
+    document.getElementById("six").style.display="none";
+    
+    document.getElementById("kk").style.display="block";
+    document.getElementById("ll").style.display="block";
+    document.getElementById("mm").style.display="block";
+    document.getElementById("nn").style.display="block";
+    document.getElementById("oo").style.display="block";
+    document.getElementById("kk").style.display="block";
+   
+     
+}
